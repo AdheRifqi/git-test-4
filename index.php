b5b28e23d5cae0ff1c3b8071dacc9cecdb3ef61b
AdheRifqi <adherifqi11@gmail.com>
2023-10-15 09:22:24 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus eveniet dolorum nisi obcaecati nihil fugit at voluptas alias! Architecto illum nisi similique inventore, qui incidunt eos eum iusto atque velit.</p>
+    <ul>
+        <li>Satu</li>
+        <li>Satu</li>
+    </ul>
 </body>
 </html>

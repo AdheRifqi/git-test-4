2fc92c07faa2e4691b7aa652b5bf9a8132efd654
AdheRifqi <adherifqi11@gmail.com>
2023-10-15 09:17:43 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus eveniet dolorum nisi obcaecati nihil fugit at voluptas alias! Architecto illum nisi similique inventore, qui incidunt eos eum iusto atque velit.</p>
 </body>
 </html>
